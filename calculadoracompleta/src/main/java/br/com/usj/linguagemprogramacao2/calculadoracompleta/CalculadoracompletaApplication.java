bf2de0163db2b6f010208d376c419eb84f76d708
FelipeHamannAndrade <67886006+FelipeHamannAndrade@users.noreply.github.com>
2020-11-22 19:11:15 -0300

    Add files via upload

---
+++ calculadoracompleta/src/main/java/br/com/usj/linguagemprogramacao2/calculadoracompleta/CalculadoracompletaApplication.java
@@ -0,0 +1,13 @@
+package br.com.usj.linguagemprogramacao2.calculadoracompleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoracompletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoracompletaApplication.class, args);
+	}
+
+}
---
+++ calculadoracompleta/src/test/java/br/com/usj/linguagemprogramacao2/calculadoracompleta/CalculadoracompletaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usj.linguagemprogramacao2.calculadoracompleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoracompletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
